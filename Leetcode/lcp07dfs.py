23fc178b057c8bbfa3dd0fefe52be7692ad1f6f7
DeveloperGG <41569137+DeveloperGG@users.noreply.github.com>
2022-06-01 23:25:42 +0800

    gyx ++

---
+++ Leetcode/lcp07dfs.py
@@ -0,0 +1,34 @@
+from ast import List
+import collections
+
+
+class Solution:
+    def __init__(self, n, relation, k):
+        self.ways, self.n, self.relation, self.k = 0, n, relation, k
+        self.edges = collections.defaultdict(list)
+
+    def numWays(self,) -> int:
+        # initialize edge as dict
+        for src, dst in self.relation:
+            self.edges[src].append(dst)
+            print(f"line 13 edge is: {self.edges}")
+
+        # start from index 0, steps 0
+        self.dfs(0,0)
+        print(self.ways) 
+
+    def dfs(self, index, steps):
+        print(f"line 21 index is: {index} and steps is {steps}")
+        if steps == self.k:
+            # if depth is k
+            if index == self.n-1:
+                # and if index == target node
+                # add 1 to ways and stop this call
+                self.ways += 1
+            return
+        for to in self.edges[index]:
+            self.dfs(to, steps+1)
+
+if __name__ == '__main__':
+    run = Solution(5, [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], 3)
+    run.numWays()
